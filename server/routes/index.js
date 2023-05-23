c6258adbf4f1545dce57f5255ea7813ec52cf19e
Bjarne Zaremba <86200869+bzporta@users.noreply.github.com>
2023-05-23 15:26:19 +0200

    Update index.js // AO1 Working

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express');
 const fs = require('fs');
 var router = express.Router();
+router.use(express.json());
 
 
 // var test = require('/media/sd/public/test.json');
 // const { response } = require('./express');
 
-
+var calib_data;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -88,14 +89,80 @@ router.post('/api/v1/do/reload', function(req, res, next){
 /*** GET Deliver current digital-in-data to the javascript-file ***/
 router.get('/api/v1/di/value', function(req, res, next){
   let digital_in_value = fs.readFileSync("/sys/devices/platform/soc/44009000.spi/spi_master/spi0/spi0.0/din");
+  console.log("digital_in_value");
   let digital_in_value_string = digital_in_value.toString();
+  console.log(digital_in_value_string);
   fs.writeFileSync("/media/sd/server/public/digital_in_value.json", digital_in_value_string);
   res.redirect('/');
 })
 
 /*** POST Analog Outputs*/
-router.post('/api/v1/ao/1', function(req, res, next){
-  
+router.post('/api/v1/ao/1', (request,response) => {
+  //let data = JSON.stringify({x: "3"})
+  //console.log(request);
+  //console.log(request.body.value);
+  analogWrite(request.body.value, 1);
+  response.json("Vielen Dank")
 })
 
+function analogWrite(value, output){
+  console.log("Value:", value);
+  if (value > 0  && value <10001){
+    value = calibrateOut(value, output);
+  }
+  if (value < 0){
+    value = 0;
+  }
+  console.log("Gerechneter Wert: ", value)
+
+  fs.writeFileSync("/sys/bus/iio/devices/iio:device0/out_voltage1_powerdown", "0");
+  fs.writeFileSync("/sys/bus/iio/devices/iio:device1/out_voltage2_powerdown", "0");
+
+  if (output == 1){
+    fs.writeFileSync("/sys/bus/iio/devices/iio:device0/out_voltage1_raw", value.toString());
+  }
+
+  if (output == 2){
+    fs.writeFileSync("/sys/bus/iio/devices/iio:device1/out_voltage2_raw", value.toString());
+  }
+
+}
+
+function readCalibrationdata(){
+  const allFileContents = fs.readFileSync('/etc/calib', 'utf-8');
+  calib_data = allFileContents.split(/\r?\n/);
+  console.log(calib_data);
+}
+
+function getCalibrationData(value){
+  return calib_data[value].split(" ", 4);
+}
+
+function calibrateOut(voltage, output){
+  readCalibrationdata();
+  if (output == 1){
+    cal_ao = getCalibrationData(5);
+  }
+  if (output == 2){
+    cal_ao = getCalibrationData(6);
+  }
+  return calcCalibrate(voltage, cal_ao);
+}
+
+function getCalibrationData(value){
+  return calib_data[value].split(' ', 4)
+}
+
+function calcCalibrate(val_uncal, calib){
+    var x1=parseInt(calib[0]);
+    var y1=parseInt(calib[1]);
+    var x2=parseInt(calib[2]);
+    var y2=parseInt(calib[3]);
+
+    var val_cal=(y2-y1)*parseInt(val_uncal-x1);
+    var val_cal=val_cal/(x2-x1);
+    var val_cal=val_cal+y1;
+
+    return parseInt(val_cal);
+}
 module.exports = router;
